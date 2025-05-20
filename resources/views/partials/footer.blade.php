7f4419ead4e59d53671bbbf4dc0de795b36a2207
MattiaNatella <mattianatella@gmail.com>
2025-05-20 15:28:08 +0200

    Enhance header and footer layout; update menu items and styles for better usability

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -26,60 +26,62 @@
     </section>
 
     <section id="links">
-        <div class="row cm-container">
-            <div class="col">
-                <h4>DC COMICS</h4>
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
+        <div class="cm-container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-4">
+                            <h4>DC COMICS</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+
+                            <h4 class="mt-4">SHOP</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-4">
+                            <h4>DC</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy (New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshop</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-4">
+                            <h4>SITES</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe Infinite</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 position-relative">
+                    <img class="dc-logo" src="{{ Vite::asset('public/img/dc-logo-bg.png') }}" alt="DC Logo">
+                </div>
             </div>
-
-            <div class="col">
-                <h4>SHOP</h4>
-                <ul>
-                    <li><a href="#">Shop DC</a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
-                </ul>
-            </div>
-
-            <div class="col">
-                <h4>DC</h4>
-                <ul>
-                    <li><a href="#">Terms Of Use</a></li>
-                    <li><a href="#">Privacy policy (New)</a></li>
-                    <li><a href="#">Ad Choices</a></li>
-                    <li><a href="#">Advertising</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Subscriptions</a></li>
-                    <li><a href="#">Talent Workshop</a></li>
-                    <li><a href="#">CPSC Certificates</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Shop Help</a></li>
-                    <li><a href="#">Contact Us</a></li>
-
-                </ul>
-
-            </div>
-
-            <div class="col">
-                <h4>SITES</h4>
-                <ul>
-                    <li><a href="#">DC</a></li>
-                    <li><a href="#">MAD Magazine</a></li>
-                    <li><a href="#">DC Kids</a></li>
-                    <li><a href="#">DC Universe Inifinite</a></li>
-                    <li><a href="#">DC Power Visa</a></li>
-
-                </ul>
-
-            </div>
-
         </div>
     </section>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,23 @@
 <header>
     <div class="header-top">
-        <div class="cm-container d-flex justify-content-between">
+        <div class="cm-container d-flex justify-content-between align-items-center">
             <div class="logo">
                 <img class="w-50" src="{{ Vite::asset('public\img\dc-logo.png') }}" alt="immagine logo DC">
             </div>
 
             <div class="menu">
                 <nav>
-                    <ul class="d-flex list-unstyled gap-2 ">
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
-                        <li>menu</li>
+                    <ul class="d-flex list-unstyled gap-2 m-0">
+                        <li>CHARACTERS</li>
+                        <li>COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
                     </ul>
                 </nav>
             </div>
